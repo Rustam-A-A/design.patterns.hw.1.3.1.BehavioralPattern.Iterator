85c70f0d4e2bb3c9e5348ef483ee47a9381c77f8
Rustam Aktuganov <aktuganov@yandex.ru>
2021-04-20 14:08:52 +0200

    initial commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    public List<Integer> randoms= new ArrayList<>();
+    protected int max;
+    protected int min;
+    protected Random random;
+
+    //формирование коллекции
+    public Randoms(int min, int max) {
+        Random random = new Random();
+        while(true){
+            int x = random.nextInt((max - min)) + min + 1;
+            randoms.add(x);
+            if (x == 100) {
+                break;
+            }
+        }
+    }
+
+    //итератор
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int next = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (next < randoms.size()) {
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                if (next < randoms.size()){
+                    Integer element = randoms.get(next);
+                    next++;
+                    return element;
+                }
+                return null;
+            }
+        };
+    }
+
+
+
+}
